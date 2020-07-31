cd277bee81c2910e44a50cf034e11d9ac64ce9fa
Randy Zwitch <rzwitch@gmail.com>
2020-07-31 14:37:43 -0400

    opencv tests

--- tests/test_package.py
+++ tests/test_package.py
@@ -1,5 +1,6 @@
 import streamlit as st
 from seleniumbase import BaseCase
+import cv2
 
 
 class ComponentsTest(BaseCase):
@@ -11,8 +12,19 @@ class ComponentsTest(BaseCase):
         # automated visual regression testing
         # https://github.com/seleniumbase/SeleniumBase/tree/master/examples/visual_testing
         # level 2 chosen, as id values dynamically generated on each page run
-        self.check_window(name="first_test)", level=2)
+        self.check_window(name="first_test", level=2)
 
         # check folium app-specific parts
         # automated test level=2 only checks structure, not content
         self.assert_text("streamlit-folium")
+
+        # test screenshots look exactly the same
+        self.save_screenshot("current-screenshot.png")
+        original = cv2.imread(
+            "visual_baseline/test_package.test_basic/first_test/screenshot.png"
+        )
+        duplicate = cv2.imread("current-screenshot.png")
+
+        difference = cv2.subtract(original, duplicate)
+        b, g, r = cv2.split(difference)
+        assert cv2.countNonZero(b) == cv2.countNonZero(g) == cv2.countNonZero(r) == 0
