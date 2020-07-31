92c137f649cb7fc2f7cb82adfb96aea8969011c0
Randy Zwitch <rzwitch@gmail.com>
2020-07-31 14:13:42 -0400

    Remove unneeded tests

--- tests/test_package.py
+++ tests/test_package.py
@@ -8,11 +8,6 @@ class ComponentsTest(BaseCase):
         # open the app
         self.open("http://localhost:8501")
 
-        # check basic properties of all Streamlit apps
-        self.assert_title("app_to_test · Streamlit")
-        self.assert_element("div.withScreencast")
-        self.assert_element("div.stApp")
-
         # automated visual regression testing
         # https://github.com/seleniumbase/SeleniumBase/tree/master/examples/visual_testing
         # level 2 chosen, as id values dynamically generated on each page run
