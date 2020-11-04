72b480c9661f00c232c8bf424f028a4fe687d6b8
Randy Zwitch <rzwitch@gmail.com>
2020-11-04 10:22:48 -0500

    Update template

--- streamlit_folium/__init__.py
+++ streamlit_folium/__init__.py
@@ -2,22 +2,59 @@ import os
 import streamlit.components.v1 as components
 import folium
 
-# Create a _RELEASE constant. We'll set this to False while we're developing
-# the component, and True when we're ready to package and distribute it.
-# (This is, of course, optional - there are innumerable ways to manage your
-# release process.)
+
+def folium_static(fig, width=700, height=500):
+
+    """
+    Renders `folium.Figure` or `folium.Map` in a Streamlit app. This method is 
+    a static Streamlit Component, meaning, no information is passed back from
+    Leaflet on browser interaction.
+
+    Parameters
+    ----------
+    width : int
+        Width of result
+
+    Height : int
+        Height of result
+
+    Note
+    ----
+    If `height` is set on a `folium.Map` or `folium.Figure` object, 
+    that value supersedes the values set with the keyword arguments of this function. 
+
+    Example
+    -------
+    >>> m = folium.Map(location=[45.5236, -122.6750])
+    >>> folium_static(m)
+
+    """
+
+    # if Map, wrap in Figure
+    if isinstance(fig, folium.Map):
+        fig = folium.Figure().add_child(fig)
+
+    return components.html(
+        fig.render(), height=(fig.height or height) + 10, width=width
+    )
+
+
+# # Create a _RELEASE constant. We'll set this to False while we're developing
+# # the component, and True when we're ready to package and distribute it.
+# # (This is, of course, optional - there are innumerable ways to manage your
+# # release process.)
 # _RELEASE = False
 
-# Declare a Streamlit component. `declare_component` returns a function
-# that is used to create instances of the component. We're naming this
-# function "_component_func", with an underscore prefix, because we don't want
-# to expose it directly to users. Instead, we will create a custom wrapper
-# function, below, that will serve as our component's public API.
+# # Declare a Streamlit component. `declare_component` returns a function
+# # that is used to create instances of the component. We're naming this
+# # function "_component_func", with an underscore prefix, because we don't want
+# # to expose it directly to users. Instead, we will create a custom wrapper
+# # function, below, that will serve as our component's public API.
 
-# It's worth noting that this call to `declare_component` is the
-# *only thing* you need to do to create the binding between Streamlit and
-# your component frontend. Everything else we do in this file is simply a
-# best practice.
+# # It's worth noting that this call to `declare_component` is the
+# # *only thing* you need to do to create the binding between Streamlit and
+# # your component frontend. Everything else we do in this file is simply a
+# # best practice.
 
 # if not _RELEASE:
 #     _component_func = components.declare_component(
@@ -39,11 +76,11 @@ import folium
 #     _component_func = components.declare_component("my_component", path=build_dir)
 
 
-# Create a wrapper function for the component. This is an optional
-# best practice - we could simply expose the component function returned by
-# `declare_component` and call it done. The wrapper allows us to customize
-# our component's API: we can pre-process its input args, post-process its
-# output value, and add a docstring for users.
+# # Create a wrapper function for the component. This is an optional
+# # best practice - we could simply expose the component function returned by
+# # `declare_component` and call it done. The wrapper allows us to customize
+# # our component's API: we can pre-process its input args, post-process its
+# # output value, and add a docstring for users.
 # def my_component(name, key=None):
 #     """Create a new instance of "my_component".
 #     Parameters
@@ -75,37 +112,30 @@ import folium
 #     return component_value
 
 
-def folium_static(fig, width=700, height=500):
+# # Add some test code to play with the component while it's in development.
+# # During development, we can run this just as we would any other Streamlit
+# # app: `$ streamlit run my_component/__init__.py`
+# if not _RELEASE:
+#     import streamlit as st
 
-    """
-    Renders `folium.Figure` or `folium.Map` in a Streamlit app. This method is 
-    a static Streamlit Component, meaning, no information is passed back from
-    Leaflet on browser interaction.
+#     st.subheader("Component with constant args")
 
-    Parameters
-    ----------
-    width : int
-        Width of result
-    
-    Height : int
-        Height of result
+#     # Create an instance of our component with a constant `name` arg, and
+#     # print its output value.
+#     num_clicks = my_component("World")
+#     st.markdown("You've clicked %s times!" % int(num_clicks))
 
-    Note
-    ----
-    If `height` is set on a `folium.Map` or `folium.Figure` object, 
-    that value supersedes the values set with the keyword arguments of this function. 
+#     st.markdown("---")
+#     st.subheader("Component with variable args")
 
-    Example
-    -------
-    >>> m = folium.Map(location=[45.5236, -122.6750])
-    >>> folium_static(m)
-
-    """
-
-    # if Map, wrap in Figure
-    if isinstance(fig, folium.Map):
-        fig = folium.Figure().add_child(fig)
-
-    return components.html(
-        fig.render(), height=(fig.height or height) + 10, width=width
-    )
+#     # Create a second instance of our component whose `name` arg will vary
+#     # based on a text_input widget.
+#     #
+#     # We use the special "key" argument to assign a fixed identity to this
+#     # component instance. By default, when a component's arguments change,
+#     # it is considered a new instance and will be re-mounted on the frontend
+#     # and lose its current state. In this case, we want to vary the component's
+#     # "name" argument without having it get recreated.
+#     name_input = st.text_input("Enter a name", value="Streamlit")
+#     num_clicks = my_component(name_input, key="foo")
+#     st.markdown("You've clicked %s times!" % int(num_clicks))
--- streamlit_folium/frontend/src/MyComponent.tsx
+++ streamlit_folium/frontend/src/MyComponent.tsx
@@ -1,9 +1,9 @@
-import React, { ReactNode } from "react"
 import {
-  withStreamlitConnection,
-  StreamlitComponentBase,
   Streamlit,
-} from "./streamlit"
+  StreamlitComponentBase,
+  withStreamlitConnection
+} from "streamlit-component-lib";
+import React, { ReactNode } from "react"
 
 interface State {
   numClicks: number
