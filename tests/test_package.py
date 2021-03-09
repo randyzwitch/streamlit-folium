ebf33a7f59433d62c8b83ef3a85f36ecb03f7e17
Randy Zwitch <rzwitch@gmail.com>
2021-03-09 13:28:23 -0500

    Set options

--- tests/test_package.py
+++ tests/test_package.py
@@ -5,6 +5,11 @@ import time
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 
+options = webdriver.ChromeOptions()
+chrome_options = Options()
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--no-sandbox")
+chrome_options.add_argument("--disable-dev-shm-usage")
 driver = webdriver.Chrome(ChromeDriverManager().install())
 
 
