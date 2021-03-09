846c1688b31585ac924315aaed66251ea841b4a0
Randy Zwitch <rzwitch@gmail.com>
2021-03-09 13:32:28 -0500

    Errors errors errors

--- tests/test_package.py
+++ tests/test_package.py
@@ -5,7 +5,7 @@ import time
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 
-options = webdriver.ChromeOptions()
+chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument("--headless")
 chrome_options.add_argument("--no-sandbox")
 chrome_options.add_argument("--disable-dev-shm-usage")
