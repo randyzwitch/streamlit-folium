c15b09c7b954d551c6f51ade232b863669176ff9
Randy Zwitch <rzwitch@gmail.com>
2021-03-09 13:34:48 -0500

    Try again

--- tests/test_package.py
+++ tests/test_package.py
@@ -9,7 +9,6 @@ chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument("--headless")
 chrome_options.add_argument("--no-sandbox")
 chrome_options.add_argument("--disable-dev-shm-usage")
-driver = webdriver.Chrome(ChromeDriverManager().install())
 
 
 class ComponentsTest(BaseCase):
