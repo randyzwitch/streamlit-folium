a7d42a0473f211af7445a6162b9d12cbb28cbf96
Randy Zwitch <rzwitch@gmail.com>
2021-03-09 13:30:11 -0500

    Remove error

--- tests/test_package.py
+++ tests/test_package.py
@@ -6,7 +6,6 @@ from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 
 options = webdriver.ChromeOptions()
-chrome_options = Options()
 chrome_options.add_argument("--headless")
 chrome_options.add_argument("--no-sandbox")
 chrome_options.add_argument("--disable-dev-shm-usage")
