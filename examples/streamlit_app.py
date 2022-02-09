68fd82b509459cb8016175c6323f7490b4d6fc4f
Randy Zwitch <rzwitch@gmail.com>
2022-02-09 14:07:16 -0500

    Update selenium baseline

--- examples/streamlit_app.py
+++ examples/streamlit_app.py
@@ -9,20 +9,35 @@ with st.echo():
     import streamlit as st
     from streamlit_folium import folium_static
     import folium
+    import branca
 
-    page = st.radio("Select map type", ["Single map", "Dual map"], index=0)
+    page = st.radio(
+        "Select map type", ["Single map", "Dual map", "Branca figure"], index=0
+    )
 
-    # center on Liberty Bell
+    # center on Liberty Bell, add marker
     if page == "Single map":
         m = folium.Map(location=[39.949610, -75.150282], zoom_start=16)
+        tooltip = "Liberty Bell"
+        folium.Marker(
+            [39.949610, -75.150282], popup="Liberty Bell", tooltip=tooltip
+        ).add_to(m)
+
     elif page == "Dual map":
         m = folium.plugins.DualMap(location=[39.949610, -75.150282], zoom_start=16)
+        tooltip = "Liberty Bell"
+        folium.Marker(
+            [39.949610, -75.150282], popup="Liberty Bell", tooltip=tooltip
+        ).add_to(m)
 
-    # add marker for Liberty Bell
-    tooltip = "Liberty Bell"
-    folium.Marker(
-        [39.949610, -75.150282], popup="Liberty Bell", tooltip=tooltip
-    ).add_to(m)
+    elif page == "Branca figure":
+        m = branca.element.Figure()
+        fm = folium.Map(location=[39.949610, -75.150282], zoom_start=16)
+        tooltip = "Liberty Bell"
+        folium.Marker(
+            [39.949610, -75.150282], popup="Liberty Bell", tooltip=tooltip
+        ).add_to(fm)
+        m.add_child(fm)
 
     # call to render Folium map in Streamlit
     folium_static(m)
