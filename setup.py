5fef25b4e1f7dcce67fb17630d05789b813af24a
Randy Zwitch <rzwitch@gmail.com>
2022-02-09 14:39:38 -0500

    Version 0.5 (#27)

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="streamlit_folium",
-    version="0.4.0",
+    version="0.5.0",
     author="Randy Zwitch",
     author_email="randy@streamlit.io",
     description="Render Folium objects in Streamlit",
@@ -12,6 +12,6 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     include_package_data=True,
     classifiers=[],
-    python_requires=">=3.6",
-    install_requires=["streamlit>=0.79", "folium>=0.11"],
+    python_requires=">=3.7",
+    install_requires=["streamlit>=1.2", "folium>=0.11"],
 )
