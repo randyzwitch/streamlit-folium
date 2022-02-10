1e2673bc9542e7b517fe53e19b8b44f719761704
Randy Zwitch <rzwitch@gmail.com>
2022-02-10 18:51:31 -0500

    Do global pip install of package vs. PyPI

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="streamlit_folium",
-    version="0.5.0",
+    version="0.6.0",
     author="Randy Zwitch",
     author_email="randy@streamlit.io",
     description="Render Folium objects in Streamlit",
