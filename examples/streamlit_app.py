9fb34fe4a19b577d1e8caa9c3870e1d5471c7629
Randy Zwitch <rzwitch@gmail.com>
2022-05-06 17:34:41 -0400

    More impressive example (#33)
    * Update reqs
    
    * EOD save
    
    * Multi-page example
    
    * Add section for parks API
    
    * Remove temp page
    
    * Update test snapshots

--- examples/streamlit_app.py
+++ examples/streamlit_app.py
@@ -1,43 +1,92 @@
 import streamlit as st
+import streamlit_folium
 
 st.set_page_config(page_title="streamlit-folium documentation")
 
+page = st.sidebar.radio(
+    "Select page", ["Home", "Bi-directional data model"], index=0
+)
+
 "# streamlit-folium"
 
-with st.echo():
+if page == "Home":
+    "streamlit-folium integrates two great open-source projects in the Python ecosystem: [Streamlit](https://streamlit.io) and [Folium](https://python-visualization.github.io/folium/)!"
 
-    import streamlit as st
-    from streamlit_folium import folium_static
-    import folium
-    import branca
+    """
+    Currently, there are two functions defined:
 
-    page = st.radio(
-        "Select map type", ["Single map", "Dual map", "Branca figure"], index=0
-    )
+    - `st_folium()`: a bi-directional Component, taking a Folium/Branca object and plotting to the Streamlit app. Upon mount/interaction with the Streamlit app, st_folium() returns a Dict with selected information including the bounding box and items clicked on
 
-    # center on Liberty Bell, add marker
-    if page == "Single map":
+    - `folium_static()`: takes a folium.Map, folium.Figure, or branca.element.Figure object and displays it in a Streamlit app.
+    
+    _Note: `folium_static()` is based on the `_repr_html()` representation created in Folium. This function should be a strict subset the of functionality of the newer `st_folium()` function._ 
+    
+    It is recommended that users switch to `st_folium()` as soon as possible, as `folium_static()` will likely be deprecated. If there is a reason why `folium_static()` needs to remain, please leave a [GitHub issue](https://github.com/randyzwitch/streamlit-folium/issues) describing your use-case.
+    """
+
+
+    "---"
+    "### Basic `st_folium()` Example"
+
+    with st.echo():
+
+        import streamlit as st
+        from streamlit_folium import st_folium
+        import folium
+    
+        # center on Liberty Bell, add marker
         m = folium.Map(location=[39.949610, -75.150282], zoom_start=16)
-        tooltip = "Liberty Bell"
         folium.Marker(
-            [39.949610, -75.150282], popup="Liberty Bell", tooltip=tooltip
+            [39.949610, -75.150282], 
+            popup="Liberty Bell", 
+            tooltip="Liberty Bell"
         ).add_to(m)
 
-    elif page == "Dual map":
-        m = folium.plugins.DualMap(location=[39.949610, -75.150282], zoom_start=16)
-        tooltip = "Liberty Bell"
+        # call to render Folium map in Streamlit
+        st_data = st_folium(m, width = 725)
+
+elif page == "Bi-directional data model":
+    """   
+    On its own, Folium is limited to _display-only_ visualizations; the Folium API generates the proper [leaflet.js](https://leafletjs.com/) specification,
+    as HTML and displays it. Some interactivity is provided (depending on how the Folium API is utilized), but the biggest drawback
+    is that the interactivity from the visualization isn't passed back to Python, and as such, you can't make full use of the functionality
+    provided by the leaflet.js library.
+
+    `streamlit-folium` builds upon the convenient [Folium API](https://python-visualization.github.io/folium/modules.html)
+    for building geospatial visualizations by adding a _bi-directional_ data transfer functionality. This not only allows for increased interactivity between
+    the web browser and Python, but also the use of larger datasets through intelligent querying.
+
+    ### Bi-directional data model
+
+    If we take a look at the example from the Home page, it might seem trivial. We define a single point with a marker and pop-up and display it: 
+    """
+    with st.echo():
+
+        import streamlit as st
+        from streamlit_folium import st_folium
+        import folium
+    
+        # center on Liberty Bell, add marker
+        m = folium.Map(location=[39.949610, -75.150282], zoom_start=16)
         folium.Marker(
-            [39.949610, -75.150282], popup="Liberty Bell", tooltip=tooltip
+            [39.949610, -75.150282], 
+            popup="Liberty Bell", 
+            tooltip="Liberty Bell"
         ).add_to(m)
 
-    elif page == "Branca figure":
-        m = branca.element.Figure()
-        fm = folium.Map(location=[39.949610, -75.150282], zoom_start=16)
-        tooltip = "Liberty Bell"
-        folium.Marker(
-            [39.949610, -75.150282], popup="Liberty Bell", tooltip=tooltip
-        ).add_to(fm)
-        m.add_child(fm)
+        # call to render Folium map in Streamlit
+        st_data = st_folium(m, width = 725)
 
-    # call to render Folium map in Streamlit
-    folium_static(m)
+    """
+    But behind the scenes, a lot more is happening _by default_. The return value of `st_folium` is set to 
+    `st_data`, and within this Python variable is information about what is being displayed on the screen:
+    """
+
+    with st.expander("Expand to see data returned to Python"):
+        st_data
+
+    """
+    As the user interacts with the data visualization, the values for `bounds` are constantly updating, along
+    with `zoom`. With these values available in Python, we can now limit queries based on bounding box, change
+    the marker size based on the `zoom` value and much more!
+    """
