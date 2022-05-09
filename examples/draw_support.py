746664f969e75b555182687ac4fc2044626c3a08
randyzwitch <rzwitch@gmail.com>
2022-05-09 16:45:57 -0400

    Clean up draw example

--- examples/draw_support.py
+++ examples/draw_support.py
@@ -3,12 +3,16 @@ import streamlit as st
 from folium.plugins import Draw
 from streamlit_folium import st_folium
 
-"# Try drawing some objects and then clicking on them"
+st.set_page_config(layout="wide")
+
+"## Try drawing some objects and then clicking on them"
 
 m = folium.Map(location=[39.949610, -75.150282], zoom_start=5)
-
 Draw(export=True).add_to(m)
 
-output = st_folium(m, width=500, height=500)
+c1, c2 = st.columns(2)
+with c1:    
+    output = st_folium(m, width = 700, height=500)
 
-st.sidebar.write(output)
+with c2:
+    st.write(output)
