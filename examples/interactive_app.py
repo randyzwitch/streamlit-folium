4dec98086160d938bf31098d68e3c5f415926671
randyzwitch <rzwitch@gmail.com>
2022-05-10 17:14:20 -0400

    Update interactive app

--- examples/interactive_app.py
+++ examples/interactive_app.py
@@ -1,11 +1,13 @@
 from dataclasses import dataclass
 from typing import Dict, List, Optional
+import time
 
 import folium
 import requests
 import streamlit as st
 from streamlit_folium import st_folium
 
+st.set_page_config(layout="wide")
 
 @st.experimental_singleton
 def get_data() -> List[Dict]:
@@ -19,7 +21,6 @@ def get_data() -> List[Dict]:
         park["_point"] = Point.from_dict(park)
 
     return parks
-
 
 @dataclass
 class Point:
@@ -58,46 +59,78 @@ class Bounds:
             Point.from_dict(data["_southWest"]), Point.from_dict(data["_northEast"])
         )
 
+############################# 
+# Streamlit app
+#############################
 
+"## National Parks in the United States"
+
+"""
+The National Parks Service provides an [API](https://www.nps.gov/subjects/digital/nps-data-api.htm) to programmatically explore NPS data. 
+
+We can take data about each park and display it on the map _conditionally_ based on whether it is in the viewport. 
+
+---
+"""
+
+# define layout
+c1, c2 = st.columns(2)
+
+# get and cache data from API
 parks = get_data()
 
-"## Click on one of the markers"
+# layout map
+with c1:
+    m = folium.Map(location=[39.949610, -75.150282], zoom_start=4)
 
-m = folium.Map(location=[39.949610, -75.150282], zoom_start=5)
+    for park in parks:
+        popup = folium.Popup(f"""
+                  <a href="{park["url"]}" target="_blank">{park["fullName"]}</a><br>
+                  <br>
+                  {park["operatingHours"][0]["description"]}<br>
+                  <br>
+                  Phone: {park["contacts"]["phoneNumbers"][0]["phoneNumber"]}<br>
+                  """,
+                  max_width = 250)
+        folium.Marker(
+            [park["latitude"], park["longitude"]], popup=popup
+        ).add_to(m)
 
-for park in parks:
-    tooltip = park["name"]
-    folium.Marker(
-        [park["latitude"], park["longitude"]], popup=park["name"], tooltip=tooltip
-    ).add_to(m)
 
+    map_data = st_folium(m, key="fig1", width=700, height=700)
 
-map_data = st_folium(m, key="fig1", width=700, height=700)
+# get data from map for further processing
+# try just used to swallow error trying to process bounds before leaflet returns it
+try:
+    map_bounds = Bounds.from_dict(map_data["bounds"])
+except TypeError:
+    time.sleep(0.1)
+    map_bounds = Bounds.from_dict(map_data["bounds"])
 
-map_bounds = Bounds.from_dict(map_data["bounds"])
-
+# when a point is clicked, display additional information about the park
 try:
     point_clicked: Optional[Point] = Point.from_dict(map_data["last_object_clicked"])
+
+    if point_clicked is not None:
+        with st.spinner(text="loading image..."):
+            for park in parks:
+                if park["_point"].is_close_to(point_clicked):
+                    with c2:
+                        f"""### _{park["fullName"]}_"""
+                        park["description"]
+                        st.image(park["images"][0]["url"], caption = park["images"][0]["caption"])
+                        st.expander("Show park full details").write(park)
 except TypeError:
     point_clicked = None
 
-if point_clicked is not None:
-    with st.spinner(text="loading image..."):
-        for park in parks:
-            if park["_point"].is_close_to(point_clicked):
-                st.image(park["images"][0]["url"])
-                st.expander("Show park full details").write(park)
+# even though there is a c1 reference above, we can do it again
+# output will get appended after original content
+with c1: 
 
-parks_in_view: List[Dict] = []
-for park in parks:
-    if map_bounds.contains_point(park["_point"]):
-        parks_in_view.append(park)
+    parks_in_view: List[Dict] = []
+    for park in parks:
+        if map_bounds.contains_point(park["_point"]):
+            parks_in_view.append(park)
 
-st.sidebar.write("## Parks visible")
-for park in parks_in_view:
-    with st.sidebar.expander(park["name"]):
-        st.write(park["description"])
-
-st.sidebar.write("---")
-
-st.sidebar.expander("Show map data").write(map_data)
+    "Parks visible:", len(parks_in_view)
+    "Bounding box:", map_bounds
