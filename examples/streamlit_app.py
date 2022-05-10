bc94db15db7e36a681a9968f223aef270de87fe1
Randy Zwitch <rzwitch@gmail.com>
2022-05-10 12:45:19 -0400

    Add Draw plugin to docs (#55)
    * Beginning of Draw page
    
    * Make sure pytest runs out of tests folder
    
    * Update test baseline
    
    * Wide mode
    
    * Pin to 1.8.1
    
    * Try again
    
    * Add latest.png
    
    * Add Draw explanation

--- examples/streamlit_app.py
+++ examples/streamlit_app.py
@@ -4,7 +4,7 @@ import streamlit_folium
 st.set_page_config(page_title="streamlit-folium documentation")
 
 page = st.sidebar.radio(
-    "Select page", ["Home", "Bi-directional data model"], index=0
+    "Select page", ["Home", "Bi-directional data model", "Plugin: Draw"], index=0
 )
 
 "# streamlit-folium"
@@ -90,3 +90,32 @@ elif page == "Bi-directional data model":
     with `zoom`. With these values available in Python, we can now limit queries based on bounding box, change
     the marker size based on the `zoom` value and much more!
     """
+
+elif page == "Plugin: Draw":
+
+    """
+    Folium supports some of the [most popular leaflet plugins](https://python-visualization.github.io/folium/plugins.html). In this example,
+    we can add the [`Draw`](https://python-visualization.github.io/folium/plugins.html#folium.plugins.Draw) plugin to our map, which allows for drawing geometric shapes on the map.
+    
+    When a shape is drawn on the map, the coordinates that represent that shape are passed back as a geojson feature via
+    the `all_drawings` and `last_active_drawing` data fields.
+
+    Draw something below to see the return value back to Streamlit!  
+    """
+
+    with st.echo():
+
+        import folium
+        import streamlit as st
+        from folium.plugins import Draw
+        from streamlit_folium import st_folium
+
+        m = folium.Map(location=[39.949610, -75.150282], zoom_start=5)
+        Draw(export=True).add_to(m)
+
+        c1, c2 = st.columns(2)
+        with c1:    
+            output = st_folium(m, width = 700, height=500)
+
+        with c2:
+            st.write(output)
