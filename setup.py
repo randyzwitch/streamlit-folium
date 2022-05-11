0cf5cf6a6d1585f52e1f68df725f28133b69d73e
Zachary Blackwood <zblackwo@gmail.com>
2022-05-11 09:19:31 -0400

    Convert initial bounds from list-style to dict-style in default

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="streamlit_folium",
-    version="0.6.7",
+    version="0.6.8",
     author="Randy Zwitch",
     author_email="randy@streamlit.io",
     description="Render Folium objects in Streamlit",
