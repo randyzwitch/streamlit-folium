4c5f69a8d0a6052324c74704f1fb42adbc7fb68b
Zachary Blackwood <zachary@streamlit.io>
2022-05-23 17:45:55 -0400

    Fix google earth engine flickering and cleanup code (#66)
    * WIP
    
    * Cleaned up TS (#65)
    
    * Make map key not depend on random google earth url, and get map populated
    
    * Cleanup and bump version
    
    * Remove print
    
    * Rename and add comments for clarity
    
    * Format
    
    Co-authored-by: Ken McGrady <ken.mcgrady@gmail.com>

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="streamlit_folium",
-    version="0.6.10",
+    version="0.6.11",
     author="Randy Zwitch",
     author_email="randy@streamlit.io",
     description="Render Folium objects in Streamlit",
--- streamlit_folium/frontend/src/index.tsx
+++ streamlit_folium/frontend/src/index.tsx
@@ -1,23 +1,98 @@
-import { Streamlit, RenderData } from "streamlit-component-lib";
-import { debounce } from "underscore";
-import { circleToPolygon } from "./circle-to-polygon";
-
-let map: any = null;
+import { Streamlit, RenderData } from "streamlit-component-lib"
+import { debounce } from "underscore"
+import { circleToPolygon } from "./circle-to-polygon"
 
 type GlobalData = {
-  map: any;
-  lat_lng_clicked: any;
-  last_object_clicked: any;
-  last_active_drawing: any,
-  all_drawings: any,
-  bounds: any;
-  zoom: any;
-  drawn_items: any;
-  last_circle_radius: number;
-  last_circle_polygon: any;
-};
+  lat_lng_clicked: any
+  last_object_clicked: any
+  last_active_drawing: any
+  all_drawings: any
+  zoom: any
+  drawn_items: any
+  last_circle_radius: number | null
+  last_circle_polygon: any
+}
 
-declare var __GLOBAL_DATA__: GlobalData;
+declare global {
+  interface Window {
+    __GLOBAL_DATA__: GlobalData
+    initComponent: any
+    map: any
+  }
+}
+
+function onMapClick(e: any) {
+  const global_data = window.__GLOBAL_DATA__
+  global_data.lat_lng_clicked = e.latlng
+  debouncedUpdateComponentValue(window.map)
+}
+
+let debouncedUpdateComponentValue = debounce(updateComponentValue, 250)
+
+function updateComponentValue(map: any) {
+  const global_data = window.__GLOBAL_DATA__
+  let bounds = map.getBounds()
+  let zoom = map.getZoom()
+  Streamlit.setComponentValue({
+    last_clicked: global_data.lat_lng_clicked,
+    last_object_clicked: global_data.last_object_clicked,
+    all_drawings: global_data.all_drawings,
+    last_active_drawing: global_data.last_active_drawing,
+    bounds: bounds,
+    zoom: zoom,
+    last_circle_radius: global_data.last_circle_radius,
+    last_circle_polygon: global_data.last_circle_polygon,
+  })
+}
+
+function onMapMove(e: any) {
+  debouncedUpdateComponentValue(window.map)
+}
+
+function onDraw(e: any) {
+  const global_data = window.__GLOBAL_DATA__
+
+  var type = e.layerType,
+    layer = e.layer
+
+  if (type === "circle") {
+    var center: [number, number] = [layer._latlng.lng, layer._latlng.lat]
+    var radius = layer.options.radius // In km
+    var polygon = circleToPolygon(center, radius)
+    global_data.last_circle_radius = radius / 1000 // Convert to km to match what UI shows
+    global_data.last_circle_polygon = polygon
+  }
+  return onLayerClick(e)
+}
+
+function onLayerClick(e: any) {
+  const global_data = window.__GLOBAL_DATA__
+  global_data.last_object_clicked = e.latlng
+  let details: Array<any> = []
+  if (e.layer && e.layer.toGeoJSON) {
+    global_data.last_active_drawing = e.layer.toGeoJSON()
+  }
+  if (global_data.drawn_items.toGeoJSON) {
+    details = global_data.drawn_items.toGeoJSON().features
+  }
+  global_data.all_drawings = details
+  debouncedUpdateComponentValue(window.map)
+}
+
+window.initComponent = (map: any) => {
+  map.on("click", onMapClick)
+  map.on("moveend", onMapMove)
+  for (let key in map._layers) {
+    let layer = map._layers[key]
+    layer.on("click", onLayerClick)
+  }
+  map.on("draw:created", onDraw)
+  map.on("draw:edited", onDraw)
+  map.on("draw:deleted", onDraw)
+
+  Streamlit.setFrameHeight()
+  updateComponentValue(map)
+}
 
 /**
  * The component's render function. This will be called immediately after
@@ -28,130 +103,51 @@ function onRender(event: Event): void {
   // Get the RenderData from the event
   const data = (event as CustomEvent<RenderData>).detail
 
-  const fig: string = data.args["fig"];
-  const height: number = data.args["height"];
-  const width: number = data.args["width"];
+  const fig: string = data.args["fig"]
+  const height: number = data.args["height"]
+  const width: number = data.args["width"]
 
-  function onMapClick(e: any) {
-    const global_data = __GLOBAL_DATA__;
-    global_data.lat_lng_clicked = e.latlng;
-    debouncedUpdateComponentValue()
-  }
-
-  let debouncedUpdateComponentValue = debounce(updateComponentValue, 250)
-
-  function updateComponentValue() {
-    const global_data = __GLOBAL_DATA__;
-    let map = global_data.map;
-    let bounds = map.getBounds();
-    let zoom = map.getZoom();
-    Streamlit.setComponentValue({
-      last_clicked: global_data.lat_lng_clicked,
-      last_object_clicked: global_data.last_object_clicked,
-      all_drawings: global_data.all_drawings,
-      last_active_drawing: global_data.last_active_drawing,
-      bounds: bounds,
-      zoom: zoom,
-      last_circle_radius: global_data.last_circle_radius,
-      last_circle_polygon: global_data.last_circle_polygon,
-    })
-  }
-
-  function onMapMove(e: any) {
-    debouncedUpdateComponentValue()
-  }
-
-  function onDraw(e: any) {
-    const global_data = __GLOBAL_DATA__;
-
-    var type = e.layerType,
-      layer = e.layer;
-
-    if (type === "circle") {
-      var center: [number, number] = [layer._latlng.lng, layer._latlng.lat];
-      var radius = layer.options.radius; // In km
-      var polygon = circleToPolygon(center, radius);
-      global_data.last_circle_radius = radius / 1000; // Convert to km to match what UI shows
-      global_data.last_circle_polygon = polygon;
+  if (!window.map) {
+    // Only run this if the map hasn't already been created (and thus the global
+    //data hasn't been initialized)
+    const div1 = document.getElementById("map_div")
+    const div2 = document.getElementById("map_div2")
+    if (div2) {
+      div2.style.height = `${height}px`
+      div2.style.width = `${width}px`
     }
-    return onLayerClick(e);
-  }
+    if (div1) {
+      div1.style.height = `${height}px`
+      div1.style.width = `${width}px`
 
-  function onLayerClick(e: any) {
-    const global_data = __GLOBAL_DATA__;
-    global_data.last_object_clicked = e.latlng;
-    let details: Array<any> = [];
-    if (e.layer && e.layer.toGeoJSON) {
-      global_data.last_active_drawing = e.layer.toGeoJSON();
-    }
-    if (global_data.drawn_items.toGeoJSON) {
-      details = global_data.drawn_items.toGeoJSON().features;
-    }
-    global_data.all_drawings = details;
-    debouncedUpdateComponentValue()
-  }
-
-  if (map == null) {
-    try {
-      map = __GLOBAL_DATA__.map;
-    } catch (e) {
-      // Only run this if the map hasn't already been created (and thus the global
-      //data hasn't been initialized)
-      const map_div = document.getElementById("map_div");
-      const map_div2 = document.getElementById("map_div2");
-      if (map_div2) {
-        map_div2.style.height = `${height}px`
-        map_div2.style.width = `${width}px`
+      if (fig.indexOf("document.getElementById('export')") !== -1) {
+        let a = document.createElement("a")
+        a.href = "#"
+        a.id = "export"
+        a.innerHTML = "Export"
+        document.body.appendChild(a)
       }
-      if (map_div) {
-        map_div.style.height = `${height}px`
-        map_div.style.width = `${width}px`
 
-        if (fig.indexOf("document.getElementById('export')") !== -1) {
-          let a = document.createElement("a");
-          a.href = "#";
-          a.id = "export";
-          a.innerHTML = "Export";
-          document.body.appendChild(a);
-        }
+      const render_script = document.createElement("script")
+      // HACK -- update the folium-generated JS to add, most importantly,
+      // the map to this global variable so that it can be used elsewhere
+      // in the script.
 
-        const render_script = document.createElement("script")
-        // HACK -- update the folium-generated JS to add, most importantly,
-        // the map to this global variable so that it can be used elsewhere
-        // in the script.
-        let set_global_data = `
-          window.__GLOBAL_DATA__ = {
-            map: map_div,
-            bounds: map_div.getBounds(),
-            lat_lng_clicked: null,
-            last_object_clicked: null,
-            all_drawings: null,
-            last_active_drawing: null,
-            zoom: null,
-            drawn_items: [],
-            last_circle_radius: null,
-            last_circle_polygon: null,
-        };`;
-        let replaced = fig + set_global_data;
-        render_script.innerHTML = replaced;
-        document.body.appendChild(render_script);
-
-        const global_data = __GLOBAL_DATA__;
-        let map = global_data.map;
-
-        map.on('click', onMapClick);
-        map.on('moveend', onMapMove);
-        for (let key in map._layers) {
-          let layer = map._layers[key];
-          layer.on("click", onLayerClick)
-        }
-        map.on('draw:created', onDraw);
-        map.on('draw:edited', onDraw);
-        map.on('draw:deleted', onDraw);
-
-        Streamlit.setFrameHeight()
-        updateComponentValue();
+      window.__GLOBAL_DATA__ = {
+        lat_lng_clicked: null,
+        last_object_clicked: null,
+        all_drawings: null,
+        last_active_drawing: null,
+        zoom: null,
+        drawn_items: [],
+        last_circle_radius: null,
+        last_circle_polygon: null,
       }
+      // The folium-generated script creates a variable called "map_div", which
+      // is the actual Leaflet map.
+      render_script.innerHTML =
+        fig + `window.map = map_div; window.initComponent(map_div);`
+      document.body.appendChild(render_script)
     }
   }
 }
