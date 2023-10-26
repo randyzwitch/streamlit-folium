56131bc7a26caeab7f78a44513fc7a59678feee4
Zachary Blackwood <zblackwo@gmail.com>
2023-10-26 13:31:44 -0400

    Bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="streamlit_folium",
-    version="0.15.0",
+    version="0.15.1",
     author="Randy Zwitch",
     author_email="rzwitch@gmail.com",
     description="Render Folium objects in Streamlit",
