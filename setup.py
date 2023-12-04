9d61a3d6560ae275f1d7f45269b530cd489556cb
Randy Zwitch <rzwitch@gmail.com>
2023-12-04 18:39:04 -0500

    Remove restriction on v0.15 of folium

--- setup.py
+++ setup.py
@@ -13,5 +13,5 @@ setuptools.setup(
     include_package_data=True,
     classifiers=[],
     python_requires=">=3.8",
-    install_requires=["streamlit>=1.13.0", "folium>=0.13,<0.15", "jinja2", "branca"],
+    install_requires=["streamlit>=1.13.0", "folium>=0.13", "jinja2", "branca"],
 )
