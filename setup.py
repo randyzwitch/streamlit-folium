43a44672cbd78cf3073d65cffd46c3a60354a130
Randy Zwitch <rzwitch@gmail.com>
2023-12-05 13:40:47 -0500

    Allow v0.15.1 of Folium (#156)
    * Remove restriction on v0.15 of folium
    
    * Change other reqs files
    
    * Explicitly exclude 0.15.0
    
    * Small tweaks in generated map code
    
    * Fix requirements.txt
    
    ---------
    
    Co-authored-by: Zachary Blackwood <zblackwo@gmail.com>

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="streamlit_folium",
-    version="0.16.0",
+    version="0.16.1",
     author="Randy Zwitch",
     author_email="rzwitch@gmail.com",
     description="Render Folium objects in Streamlit",
@@ -13,5 +13,5 @@ setuptools.setup(
     include_package_data=True,
     classifiers=[],
     python_requires=">=3.8",
-    install_requires=["streamlit>=1.13.0", "folium>=0.13,<0.15", "jinja2", "branca"],
+    install_requires=["streamlit>=1.13.0", "folium>=0.13,!=0.15.0", "jinja2", "branca"],
 )
