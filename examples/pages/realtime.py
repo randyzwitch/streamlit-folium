ce82c39e989cf8ff2d1acf1eaa5fe465598edc25
Randy Zwitch <rzwitch@gmail.com>
2024-05-07 10:05:50 -0400

    Echo realtime example code

--- examples/pages/realtime.py
+++ examples/pages/realtime.py
@@ -20,76 +20,77 @@ Inside the `JsCode` object you have access to the Streamlit object,
 which allows you to set a return value for your Streamlit app.
 """
 
-m = folium.Map()
-source = folium.JsCode(
-    """
-    function(responseHandler, errorHandler) {
-        var url = 'https://api.wheretheiss.at/v1/satellites/25544';
-
-        fetch(url)
-        .then((response) => {
-            return response.json().then((data) => {
-                var { id, timestamp, longitude, latitude } = data;
-
-                return {
-                    'type': 'FeatureCollection',
-                    'features': [{
-                        'type': 'Feature',
-                        'geometry': {
-                            'type': 'Point',
-                            'coordinates': [longitude, latitude]
-                        },
-                        'properties': {
-                            'id': id,
-                            'timestamp': timestamp
-                        }
-                    }]
-                };
+with st.echo():
+    m = folium.Map()
+    source = folium.JsCode(
+        """
+        function(responseHandler, errorHandler) {
+            var url = 'https://api.wheretheiss.at/v1/satellites/25544';
+    
+            fetch(url)
+            .then((response) => {
+                return response.json().then((data) => {
+                    var { id, timestamp, longitude, latitude } = data;
+    
+                    return {
+                        'type': 'FeatureCollection',
+                        'features': [{
+                            'type': 'Feature',
+                            'geometry': {
+                                'type': 'Point',
+                                'coordinates': [longitude, latitude]
+                            },
+                            'properties': {
+                                'id': id,
+                                'timestamp': timestamp
+                            }
+                        }]
+                    };
+                })
             })
-        })
-        .then(responseHandler)
-        .catch(errorHandler);
-    }
-"""
-)
-
-on_each_feature = folium.JsCode(
-    """
-    (feature, layer) => {
-        layer.bindTooltip(`${feature.properties.timestamp}`);
-        layer.on("click", (event) => {
-            Streamlit.setComponentValue({
-                id: feature.properties.id,
-                // Be careful, on_each_feature binds only once.
-                // You need to extract the current location from
-                // the event.
-                location: event.sourceTarget.feature.geometry
-            });
-        });
-
-    }
-"""
-)
-
-update_feature = folium.JsCode(
-    """
-    (feature, layer) => {
-        L.Realtime.prototype.options.updateFeature(feature, layer);
-        if(layer) {
-            layer.unbindTooltip();
-            layer.bindTooltip(`${feature.properties.timestamp}`);
+            .then(responseHandler)
+            .catch(errorHandler);
         }
-    }
-"""
-)
-
-Realtime(
-    source,
-    on_each_feature=on_each_feature,
-    update_feature=update_feature,
-    interval=10000,
-).add_to(m)
-
-data = st_folium(m, returned_objects=[], debug=False)
-
-st.write(data)
+    """
+    )
+    
+    on_each_feature = folium.JsCode(
+        """
+        (feature, layer) => {
+            layer.bindTooltip(`${feature.properties.timestamp}`);
+            layer.on("click", (event) => {
+                Streamlit.setComponentValue({
+                    id: feature.properties.id,
+                    // Be careful, on_each_feature binds only once.
+                    // You need to extract the current location from
+                    // the event.
+                    location: event.sourceTarget.feature.geometry
+                });
+            });
+    
+        }
+    """
+    )
+    
+    update_feature = folium.JsCode(
+        """
+        (feature, layer) => {
+            L.Realtime.prototype.options.updateFeature(feature, layer);
+            if(layer) {
+                layer.unbindTooltip();
+                layer.bindTooltip(`${feature.properties.timestamp}`);
+            }
+        }
+    """
+    )
+    
+    Realtime(
+        source,
+        on_each_feature=on_each_feature,
+        update_feature=update_feature,
+        interval=10000,
+    ).add_to(m)
+    
+    data = st_folium(m, returned_objects=[], debug=False)
+    
+    st.write(data)
