e04ac852639994e6c708ffbe19a2840f20437026
Randy Zwitch <rzwitch@gmail.com>
2024-07-23 11:34:42 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="streamlit_folium",
-    version="0.21.1",
+    version="0.22.0",
     author="Randy Zwitch",
     author_email="rzwitch@gmail.com",
     description="Render Folium objects in Streamlit",
