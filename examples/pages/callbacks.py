11ad288c4d2db0ea42513b091c69d6e1413aa911
Zachary Blackwood <zachary@streamlit.io>
2025-01-08 13:55:13 -0500

    Add forgotten demo page for on_change (#243)
    * Support on_change callback functions and if custom key is passed use it in session state
    
    * Use hash key
    
    * Update tests for indentation
    
    * Dedent
    
    * Add callbacks demo

---
+++ examples/pages/callbacks.py
@@ -0,0 +1,33 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="streamlit-folium documentation",
+    page_icon=":world_map:️",
+    layout="wide",
+)
+
+
+"# Callbacks"
+
+"""
+Try moving the map and zooming in and out and see the callback in action.
+"""
+
+with st.echo():
+    import folium
+    import streamlit as st
+
+    from streamlit_folium import st_folium
+
+    # center on Liberty Bell, add marker
+    m = folium.Map(location=[39.949610, -75.150282], zoom_start=16)
+    folium.Marker(
+        [39.949610, -75.150282], popup="Liberty Bell", tooltip="Liberty Bell"
+    ).add_to(m)
+
+    def callback():
+        st.toast(f"Current zoom: {st.session_state['my_map']['zoom']}")
+        st.toast(f"Current center: {st.session_state['my_map']['center']}")
+
+    # call to render Folium map in Streamlit
+    st_data = st_folium(m, width=725, key="my_map", on_change=callback)
